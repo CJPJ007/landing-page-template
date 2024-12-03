18cd94996bbb7cf5b9f3ebfc032bf56c51053783
Jaypal Chauhan <jaypalchauhan@Jaypals-MacBook-Air.local>
2024-12-03 17:53:00 +0530

    Navbar tags changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto flex justify-between items-center px-4">
         <div className="text-xl font-bold">YourLogo</div>
         <ul className="hidden md:flex space-x-6">
-          <li><a href="#" className="hover:text-blue-400">Home</a></li>
-          <li><a href="#" className="hover:text-blue-400">Features</a></li>
-          <li><a href="#" className="hover:text-blue-400">Pricing</a></li>
-          <li><a href="#" className="hover:text-blue-400">Contact</a></li>
+          <li><a href="/" className="hover:text-blue-400">Home</a></li>
+          <li><a href="/" className="hover:text-blue-400">Features</a></li>
+          <li><a href="/" className="hover:text-blue-400">Pricing</a></li>
+          <li><a href="/" className="hover:text-blue-400">Contact</a></li>
         </ul>
         <div className="flex space-x-4 items-center">
           <DarkModeToggle />
